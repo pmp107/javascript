d670b717aa5a722132d2815f443b1b508e77f92a
pmp107 <pmp441996@gmail.com>
2023-01-22 19:55:35 +0530

    final commit of quiz project

---
+++ index.js
@@ -0,0 +1,29 @@
+const correctAnswer = ["D", "B", "C", "B", "D"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+const questions = document.querySelectorAll(".question");
+
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+    // const userAnswers = [`${mainForm.q1.value}`,`${mainForm.q2.value}`,`${mainForm.q3.value}`,`${mainForm.q4.value}`,`${mainForm.q5.value}`]; 
+    
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswer[index]){
+            console.log(answer+'=> ' +correctAnswer[index]);
+            score += 1;
+            questions[index].classList.add("correct");
+            questions[index].classList.remove("wrong");
+        } else {
+            questions[index].classList.add("wrong");
+            questions[index].classList.remove("correct");
+        }
+    });
+console.log(score);
+    scrollTo(0, 0);
+    result.classList.remove("hide");
+    result.querySelector("p").textContent = `You scored ${score}/5!`;
+});
