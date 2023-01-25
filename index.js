d95d17a713d2a288f20c24354f1f5e0b948cd1dc
pmp107 <pmp441996@gmail.com>
2023-01-25 10:30:41 +0530

    todo list final main commit

--- index.js
+++ index.js
@@ -1,32 +1,82 @@
-const correctAnswer = ["D", "B", "C", "B", "D"];
-const form = document.querySelector(".quiz-form");
-const result = document.querySelector(".result");
-const questions = document.querySelectorAll(".question");
+const addForm = document.querySelector(".add");
+const tasks = document.querySelector(".tasks");
+const clearAll = document.querySelector(".clear");
+const messageSpan = document.querySelector(".message span");
+const searchForm = document.querySelector(".search");
 
+//function to maintain count of Task.
+function updateMessage(){
+    const textLength = tasks.children.length;
+    messageSpan.textContent = `You have ${textLength} pending tasks.`;
+}
+updateMessage();
 
-form.addEventListener("submit", event => {
+//To add new Task
+addForm.addEventListener("submit", event => {
     event.preventDefault();
-    
-    let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
-    // const userAnswers = [`${mainForm.q1.value}`,`${mainForm.q2.value}`,`${mainForm.q3.value}`,`${mainForm.q4.value}`,`${mainForm.q5.value}`]; 
-    
-    userAnswers.forEach((answer, index) => {
-        if(answer === correctAnswer[index]){
-            console.log(answer+'=> ' +correctAnswer[index]);
-            score += 1;
-            questions[index].classList.add("correct");
-        } else {
-            questions[index].classList.add("wrong");
-        }
-    });
-console.log(score);
-    scrollTo(0, 0);
-    result.classList.remove("hide");
-    result.querySelector("p").textContent = `You scored ${score}/5!`;
-    //To disable form elements once submitted.
-    Array.from(form.elements).forEach(formElement => {
-        console.log(formElement);
-        formElement.disabled = true
-    });
+    const value = addForm.task.value.trim()
+
+    if(value.length){
+        tasks.innerHTML += `<li>
+                                <span>${value}</span>
+                                <i class="bi bi-trash-fill delete"></i>
+                            </li>`;
+        addForm.reset();
+        updateMessage();
+    }
 });
+
+//To delete Task
+tasks.addEventListener("click", event => {
+    if(event.target.classList.contains("delete")){
+        event.target.parentElement.remove();
+        updateMessage();
+    }
+});
+
+//To delete all Tasks
+clearAll.addEventListener("click", event => {
+    const taskItems = tasks.querySelectorAll("li");
+    taskItems.forEach(item => {
+        item.remove();
+    });
+    updateMessage();
+});
+
+//To search specific tasks
+function filterTask(term){
+    // console.log(term);
+    //Will hide the tasks which don't match with user search string.
+    Array.from(tasks.children)
+    .filter(task => {
+        return !task.textContent.toLowerCase().includes(term);
+    })
+    .forEach(task => {
+        task.classList.add("hide");
+    });
+    //Will show the tasks which matches with user search string.
+    Array.from(tasks.children)
+    .filter(task => {
+        return task.textContent.toLowerCase().includes(term);
+    })
+    .forEach(task => {
+        // console.log(task);
+        task.classList.remove("hide");
+    });
+
+}
+
+
+searchForm.addEventListener("keyup", event => {
+    const term = searchForm.task.value.trim().toLowerCase();
+    filterTask(term);
+});
+
+
+searchForm.addEventListener("click", event => {
+    if(event.target.classList.contains("reset")){
+        searchForm.reset();
+        const term = searchForm.task.value.trim();
+        filterTask(term);
+    }
+})
