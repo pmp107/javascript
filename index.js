0b6b387f33669cef13cad99243c7232ee425cf97
pmp107 <pmp441996@gmail.com>
2023-01-28 16:03:38 +0530

    budget tracker main commit

--- index.js
+++ index.js
@@ -1,32 +1,111 @@
-const correctAnswer = ["D", "B", "C", "B", "D"];
-const form = document.querySelector(".quiz-form");
-const result = document.querySelector(".result");
-const questions = document.querySelectorAll(".question");
+const form = document.querySelector(".add");
+const incomeList = document.querySelector("ul.income-list");
+const expenseList = document.querySelector("ul.expense-list");
 
+const balance = document.getElementById("balance");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
+
+let transactions = localStorage.getItem("transactions") !== null ? JSON.parse(localStorage.getItem("transactions")) : [];
+
+
+function updateStatistics(){
+    const updatedIncome = transactions
+                            .filter(transaction => transaction.amount > 0)
+                            .reduce((total, transaction) => total += transaction.amount, 0);
+
+    const updatedExpense = transactions
+                            .filter(transaction => transaction.amount < 0)
+                            .reduce((total, transaction) => total += Math.abs(transaction.amount), 0);
+
+    updatedBalance = updatedIncome - updatedExpense;
+    balance.textContent = updatedBalance;
+    income.textContent = updatedIncome;
+    expense.textContent = updatedExpense;
+    
+}
+
+//To dynamically generate the Income/Expense lists. 
+function generateTemplate(id, source, amount, time){
+    return `<li data-id="${id}">
+                <p>
+                    <span>${source}</span>
+                    <span id="time">${time}</span>
+                </p>
+                $<span>${Math.abs(amount)}</span>
+                <i class="bi bi-trash delete"></i>
+            </li>`;
+}
+
+function addTransactionDOM(id, source, amount, time){
+    if(amount > 0){
+        incomeList.innerHTML += generateTemplate(id, source, amount, time);
+    } else {
+        expenseList.innerHTML += generateTemplate(id, source, amount, time);
+    }
+}
+
+function addTransaction(source, amount){
+    const time = new Date();
+    const transaction = {
+        id: Math.floor(Math.random()*100000),
+        source: source,
+        amount: amount,
+        time: `${time.toLocaleTimeString()} ${time.toLocaleDateString()}`
+    };
+    transactions.push(transaction);
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+    addTransactionDOM(transaction.id, source, amount, transaction.time);
+}
 
 form.addEventListener("submit", event => {
     event.preventDefault();
-    
-    let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
-    // const userAnswers = [`${mainForm.q1.value}`,`${mainForm.q2.value}`,`${mainForm.q3.value}`,`${mainForm.q4.value}`,`${mainForm.q5.value}`]; 
-    
-    userAnswers.forEach((answer, index) => {
-        if(answer === correctAnswer[index]){
-            console.log(answer+'=> ' +correctAnswer[index]);
-            score += 1;
-            questions[index].classList.add("correct");
+    if(form.source.value.trim() === "" || form.amount.value === ""){
+        return alert("Please add proper values!");
+    }
+    addTransaction(form.source.value.trim(), Number(form.amount.value));
+    updateStatistics();
+    form.reset();
+})
+
+//To fetch all the transactions which are stored in local storage.
+function getTransaction(){
+    transactions.forEach(transaction => {
+        if(transaction.amount > 0){
+            incomeList.innerHTML += generateTemplate(transaction.id, transaction.source, transaction.amount, transaction.time);
         } else {
-            questions[index].classList.add("wrong");
+            expenseList.innerHTML += generateTemplate(transaction.id, transaction.source, transaction.amount, transaction.time);
         }
     });
-console.log(score);
-    scrollTo(0, 0);
-    result.classList.remove("hide");
-    result.querySelector("p").textContent = `You scored ${score}/5!`;
-    //To disable form elements once submitted.
-    Array.from(form.elements).forEach(formElement => {
-        console.log(formElement);
-        formElement.disabled = true
+}
+
+function deleteTransaction(id){
+    transactions = transactions.filter(transaction => {
+        return transaction.id !== id;
     });
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+incomeList.addEventListener("click", event => {
+    if(event.target.classList.contains("delete")){
+        event.target.parentElement.remove();
+        deleteTransaction(Number(event.target.parentElement.dataset.id));
+        updateStatistics();
+    }
 });
+
+expenseList.addEventListener("click", event => {
+    if(event.target.classList.contains("delete")){
+        event.target.parentElement.remove();
+        deleteTransaction(Number(event.target.parentElement.dataset.id));
+        updateStatistics();
+    }
+});
+
+
+function init(){
+    updateStatistics();
+    getTransaction();
+}
+
+init();
